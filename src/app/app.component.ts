ca939726a5bb6ad101a3bdf2efc8f63114ac377a
vladimirvorovalnitsev <vladimirvorovalnitsev@gmail.com>
2019-09-03 13:37:16 +0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent implements OnInit {
+  title = 'csv-to-table';
+  // хранит названия столбцов
+  columns: string[] = [];
+  // данные для отображения
+  data: Object[] = [];
+  // источник данных для таблицы
+  dataSource = new MatTableDataSource<Object>(this.data);
+  // пагинация страниц
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  ngOnInit() {
+    // добавляем к источнику данных пагинацию
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  // функция обработчик события изменения формы с выбором файла
+  uploadListener(event: any): void {
+    let files = event.target.files;
+    // проверка типа файла
+    if (files[0].name.endsWith('.csv') && files[0].type === 'text/csv') {
+      this.data = [];
+      // создаем объект FileReader для получения содержимого файла
+      let reader = new FileReader();
+      reader.readAsText(files[0]);
+      reader.onload = () => {
+        // полученную информацию делим на строки
+        let rows: string[] = (<string>reader.result).split('\n');
+        // из первой строки получаем данные для заголовков столбцов 
+        this.columns = rows[0].split(',');
+        // из оставшихся строк получеам содержимое ячеек, собираем объект для строки таблицы и заносим его в data
+        for (let i = 1; i < rows.length - 1; i++) {
+          let row: string[] = rows[i].split(',');
+          let obj = new Object();
+          for (let j = 0; j < row.length; j++) {
+            obj[this.columns[j]] = row[j];
+          }
+          console.log(obj);
+          this.data.push(obj);
+        }
+          console.log(this.data);
+          this.dataSource.data = this.data;
+          console.log(this.dataSource.data);
+      };
+    }
+  }
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { AppComponent } from './app.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
